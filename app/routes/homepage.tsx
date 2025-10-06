304b5fef68cb6d679ec20d65d8f377baafcf105c
donkdevm2b <donkdevm2b@gmail.com>
2025-10-06 18:00:06 +0200

    edit homepage

--- app/routes/homepage.tsx
+++ app/routes/homepage.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/homepage";
-import { Welcome } from "../welcome/welcome";
 import { appTitle } from "@/lib/constants/uiText";
+import { Card, CardContent } from "@/components/ui/card"
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -9,6 +9,14 @@ export function meta({ }: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />; {/* todo: remove welcome*/ }
+export default () => {
+  return <div className="flex items-center justify-center h-screen bg-background">
+    <Card className="shadow-lg border rounded-2xl">
+      <CardContent className="p-10">
+        <h1 className="text-5xl font-bold text-center tracking-tight">
+          NewtonThrust
+        </h1>
+      </CardContent>
+    </Card>
+  </div>
 }
