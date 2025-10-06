e716f1ad057432676f9905c4af9a6f492212340d
donkdevm2b <donkdevm2b@gmail.com>
2025-10-06 09:37:48 +0200

    add user route

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,15 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    // HOMEPAGE
+    index("routes/homepage.tsx"),
 
     /* GUEST AREA */
-    // route("login", "pages/auth/login.tsx")
-    route("login", "routes/login.tsx")
+    route("login", "routes/login.tsx"),
     /* GUEST AREA */
+
+    /* USER AREA */
+    route("user", "routes/user/user.tsx")
+    /* USER AREA */
 
 ] satisfies RouteConfig;
