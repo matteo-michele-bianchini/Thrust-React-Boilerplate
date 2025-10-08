2fe799c1d54343b46048bfcdf3842b7e1405d24c
donkdevm2b <donkdevm2b@gmail.com>
2025-10-08 09:39:05 +0200

    add eslint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+// eslint-disable-next-line no-empty-pattern
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
