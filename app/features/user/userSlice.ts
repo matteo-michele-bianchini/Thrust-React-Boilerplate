43886ec6614434cf3670fce7b836caa37971cea1
donkdevm2b <donkdevm2b@gmail.com>
2025-10-13 12:26:31 +0200

    add user reducer

---
+++ app/features/user/userSlice.ts
@@ -0,0 +1,27 @@
+import type User from '@/types/user'
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface UserState {
+    data: User | null
+}
+
+const initialState: UserState = {
+    data: null
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<User>) => {
+            state.data = action.payload
+        },
+        deleteUser: (state) => {
+            state.data = null
+        },
+    },
+})
+
+export const { setUser, deleteUser } = userSlice.actions
+export default userSlice.reducer
