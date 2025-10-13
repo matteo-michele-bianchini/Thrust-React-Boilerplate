5a81872cb7b111c944bee225545d9e41cb44aeac
donkdevm2b <donkdevm2b@gmail.com>
2025-10-13 16:42:38 +0200

    react eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,12 @@
 import eslint from '@eslint/js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
 
 export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
     globalIgnores(['.husky', '.react-router', 'build', 'node_modules', 'public']),
     {
         rules: {
@@ -23,11 +25,12 @@ export default defineConfig(
             quotes: ['error', 'single', { avoidEscape: true }]
 
         },
+
     },
     {
         files: ['app/components/**'],
         rules: {
             quotes: 'off'
         }
-    }
+    },
 )
