cd251cc2fb1e37ba86fb3e871b49303669c5ec92
donkdevm2b <donkdevm2b@gmail.com>
2025-10-15 09:31:31 +0200

    e2e launcher

--- operations/mac/launchAll.sh
+++ operations/mac/launchAll.sh
@@ -3,10 +3,12 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 SCRIPT1="$SCRIPT_PATH/launchApp.sh"
 SCRIPT2="$SCRIPT_PATH/launchTest.sh"
+SCRIPT3="$SCRIPT_PATH/launchTestE2e.sh"
 
 echo "Launching scripts..."
 echo "Script 1: $SCRIPT1"
 echo "Script 2: $SCRIPT2"
+echo "Script 3: $SCRIPT3"
 
 osascript <<EOF
 tell application "Terminal"
@@ -14,5 +16,8 @@ tell application "Terminal"
     tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
     delay 0.5
     do script "source '$SCRIPT2'" in front window
+    tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
+    delay 0.5
+    do script "source '$SCRIPT3'" in front window
 end tell
 EOF
