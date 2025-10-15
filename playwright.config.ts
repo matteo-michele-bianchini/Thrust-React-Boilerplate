8fa281cf5233f4751dde3ab4467b353f8a7f3461
donkdevm2b <donkdevm2b@gmail.com>
2025-10-15 08:46:18 +0200

    add test scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 3_000,
+  // timeout: 3_000,
   testDir: './tests/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
