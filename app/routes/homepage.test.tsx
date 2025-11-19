874c822e01d424568205a0e6aea4af5940050262
matteo-michele-bianchini <donkdevm2b+git@gmail.com>
2025-11-19 14:48:08 +0100

    add memory router to tests

--- app/routes/homepage.test.tsx
+++ app/routes/homepage.test.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
 import Homepage from '@/routes/homepage'
-
 import { projectName } from '@/utils/constants/uiText'
 
 test('First test', () => {
     render(
-        <Homepage />
+        <MemoryRouter>
+            <Homepage />
+        </MemoryRouter>
     )
     expect(screen.getByText(projectName)).toBeInTheDocument()
 })
